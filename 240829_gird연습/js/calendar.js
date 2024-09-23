5494451253a7250683a02e818fc73275d0d027b5
오지은 <w2317@e-mirim.hs.kr>
2024-09-23 14:57:55 +0900

    이전달 다음달 전환 함수

--- 240829_gird연습/js/calendar.js
+++ 240829_gird연습/js/calendar.js
@@ -1,24 +1,27 @@
-// 이전/다음 버튼 클릭하면 이전달/다음달로 변경
+const calendarHeader = document.getElementById('calendar-header')
+const calendarTitle = calendarHeader.getElementsByTagName('h1')[0]
+
+// addEventListener는 (이벤트, 리턴 값) -> 호출하면 안 되고 리턴 값이 있어야 하기 때문에 arrow function 사용
+const prevMonthButton = document.getElementById('prev-month')
+prevMonthButton.addEventListener('click', () => changeMonth(-1))
+
+const nextMonthButton = document.getElementById('next-month')
+nextMonthButton.onclick = () => changeMonth(1)
 
 // 현재 날짜 구하기
 var currentDate = new Date()
 
-// 년 구하기
-const year = currentDate.getFullYear()
+// click event 발생했을 때, 해야할 일 정하자
+const changeMonth = (diff) => {
+    currentDate.setMonth(currentDate.getMonth() + diff)
 
-// 월 구하기
-const month = currentDate.getMonth()
+    // 년 구하기
+    const year = currentDate.getFullYear()
 
-// 제목 표시
+    // 월 구하기
+    const month = currentDate.getMonth()
 
-const calendarHeader = document.getElementById('calendar-header')
-const calendarTitle = calendarHeader.getElementsByTagName('h1')[0]
-// const calendarTitle = document.querySelector("#calendar-header h1")
-
-calendarTitle.innerText = `${year}년 ${month + 1}월`
-// calendarTitle.innerHTML = `<h1>${year}년 ${month + 1}월</h1>`
-
-const changeDate = (date) => {
-    currentDate.setDate(currentDate.getDate() + date)
     calendarTitle.innerText = `${year}년 ${month + 1}월`
 }
+
+changeMonth(0)
