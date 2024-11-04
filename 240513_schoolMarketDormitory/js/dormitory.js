8e7f4618b16c573f4ed6ac5f94bd273e7638ac59
오지은 <w2317@e-mirim.hs.kr>
2024-11-04 15:06:03 +0900

    dormitory get allData, weekly-reservation

--- 240513_schoolMarketDormitory/js/dormitory.js
+++ 240513_schoolMarketDormitory/js/dormitory.js
@@ -12,6 +12,39 @@ console.log(selectionItemDivs);
 // calendarDiv.style.display = "block";
 // selectionWashingMachineTimeDiv.style.display = "block";
 
+let allData; // 모든 초기화 정보 : 세탁기, 시간, 호실 정보
+let weeklyReservation; // 미리 요일별로 지정된 예약 정보
+let newReservation; // 사용자가 입력하고 있는 예약 정보
+let reservations; // 사용자가 에약 완료한 정보들 저장
+
+const initData = async () => {
+    // allData 가져오기
+    const getAllData = async (url) => {
+        return fetch(url)
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => console.log(err))
+    }
+    // weeklyReservation 가져오기
+    const getWeeklyReservation = async (url) => {
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            return data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    allData = await getAllData("js/allData.json")
+    console.log(allData)
+    weeklyReservation = await getWeeklyReservation("js/weekly-reservation.json")
+    console.log(weeklyReservation)
+}
+
+
+
+
 
 const setPage = (page) => {
     // clear select
@@ -19,6 +52,7 @@ const setPage = (page) => {
     //     selectionItem.classList.remove("select")
     // })
 
+    // 버튼 클릭 조절
     for(selectionItem of selectionItemDivs){
         selectionItem.classList.remove("select")
     }
@@ -38,3 +72,4 @@ const setPage = (page) => {
 
 
 setPage(1)
+initData()
