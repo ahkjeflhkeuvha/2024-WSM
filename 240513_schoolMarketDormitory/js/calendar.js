af94803aedc8be0b5b5ef71ed54e13d268a4217c
오지은 <w2317@e-mirim.hs.kr>
2024-11-11 13:33:38 +0900

    last class change things

--- 240513_schoolMarketDormitory/js/calendar.js
+++ 240513_schoolMarketDormitory/js/calendar.js
@@ -1,109 +1,114 @@
-const calendarHeader = document.getElementById('calendar-header')
-const calendarTitle = calendarHeader.getElementsByTagName('h1')[0]
-// const calendarTitle = document.querySelector('#calendar-header h1')
+// 현재 날짜 구하자
+currentDate = new Date();
 
-const calendarContainer = document.querySelector('#calendar-container')
+// html -> js 변수 가져오기
+const calendarHeader = document.getElementById("calendar-header");
+const calendarHeaderH1 = calendarHeader.getElementsByTagName("h1")[0];
+// const tempH1 = document.querySelector("#calender-header h1");
 
-// addEventListener는 (이벤트, 리턴 값) -> 호출하면 안 되고 리턴 값이 있어야 하기 때문에 arrow function 사용
-const prevMonthButton = document.getElementById('prev-month')
-prevMonthButton.addEventListener('click', () => changeMonth(-1))
+const calendarContainerDiv = document.querySelector("#calendar-container");
 
-const nextMonthButton = document.getElementById('next-month')
-nextMonthButton.onclick = () => changeMonth(1)
-
-// 현재 날짜 구하기
-var currentDate = new Date()
-
+// 이전/다음 버튼을 클릭 했을 때 , 이전 달/다음 달로 변경하자
+// html -> js 변수
 // click event 발생했을 때, 해야할 일 정하자
+const prevMonthBtn = document.getElementById("prev-month");
+// prevMonthBtn.addEventListener("click", console.log("이전"));
+prevMonthBtn.addEventListener("click", () => changeMonth(-1));
+
+const nextMonthBtn = document.querySelector("#next-month");
+nextMonthBtn.onclick = () => changeMonth(1);
+
+// -1 : 이전 달, 0: 현재 달, 1: 다음 달
 const changeMonth = (diff) => {
-    currentDate.setMonth(currentDate.getMonth() + diff)
-
-    // 년 구하기
-    const year = currentDate.getFullYear()
-
+    currentDate.setMonth(currentDate.getMonth() + diff);
+    // 년도 구하기
+    const year = currentDate.getFullYear();
     // 월 구하기
-    const month = currentDate.getMonth()
-
-    calendarTitle.innerText = `${year}년 ${month + 1}월`
-
+    const month = currentDate.getMonth();
+    // 제목 바꾸기
+    console.log(`${year}년 ${month + 1}월`);
+    // js 변수에 innerHTML = `${year}년 ${month + 1}월`
+    calendarHeaderH1.innerHTML = `<i>${year}년 ${month + 1}월</i>`;
     // 달력 새로 그리자
-    setCalendar(currentDate)
+    setCalendar(currentDate);
 }
 
 const setCalendar = (date) => {
-    // 이번 달 마지막 날짜 -> 다음 달 1일의 전날 => 년, 월, 월 + 1, -1
-    const year = date.getFullYear()
-    const month = date.getMonth()
+    // 현재 년
+    const year = date.getFullYear();
+    // 현재 월
+    const month = date.getMonth();
+    // 이번 달 마지막 날짜
+    const lastDate = new Date(year, month + 1, 0);  // 다음 달 1일의 전날 => 년, 월 + 1, 1 -1
+    const lastDateDate = lastDate.getDate();
+    // 이번 달 마지막 날짜의 요일
+    const lastDay = lastDate.getDay();
+    // 이번 달 마지막 날짜
+    const prevMonthLastDate = new Date(year, month, 0);
+    const prevMonthLastDateDate = prevMonthLastDate.getDate();
+    // 이번 달 첫날의 요일
+    const firstDay = new Date(year, month, 1).getDay();
 
-    const lastDate = new Date(year, month + 1, 0)
-    const lastDateDate = lastDate.getDate()
-    const lastDay = lastDate.getDay()
 
-    /* let weekName = `<div class="item week-name">일</div>
-    <div class="item week-name">월</div>
-    <div class="item week-name">화</div>
-    <div class="item week-name">수</div>
-    <div class="item week-name">목</div>
-    <div class="item week-name">금</d/iv>
-    <div class="item week-name">토</div>`
-    calendarContainer.innerHTML = weekName */
 
-    const weekNames = "일월화수목금토"
-    const weekNamesArray = weekNames.split("")
-    let weekNameString = ""
+    // let weekNameString = `<div class="item week-name">일</div>
+    //         <div class="item week-name">월</div>
+    //         <div class="item week-name">화</div>
+    //         <div class="item week-name">수</div>
+    //         <div class="item week-name">목</div>
+    //         <div class="item week-name">금</div>
+    //         <div class="item week-name">토</div>`;
 
-    weekNamesArray.forEach((val) => {
-        weekNameString += `<div class=item week-name">${val}</div>\n`
-    })
+    // calendarContainerDiv.innerHTML = weekNameString;
 
-    calendarContainer.innerHTML = weekNameString
+    let weekNameString = "";
+    const weekNames = "일월화수목금토"; // 주 이름을 문자열로 정의
+    const weekNamesArray = weekNames.split(""); // -> ["일", "월", "화", "수", "목", "금", "토"] 배열로 변환
 
-    // 이전 달의 뒷날짜 표시
-    const prevMonthLastDate = new Date(year, month, 0) // 저번 달의 마지막 날짜
-    const prevMonthLastDateDate = prevMonthLastDate.getDate()
-    const firstDay = new Date(year, month, 1).getDay()
+    // 주 이름을 포함한 div 문자열을 하나씩 weekNameString에 추가
+    weekNamesArray.forEach((weekName) => {
+        weekNameString += `<div class="item week-name">${weekName}</div>`;
+    });
 
+    // 최종 문자열을 calendarContainerDiv에 삽입
+    calendarContainerDiv.innerHTML = weekNameString;
+
+    
+
+    // 이전 달의 뒷 날짜 표시하자
     for (let date = prevMonthLastDateDate - firstDay + 1; date <= prevMonthLastDateDate; date++) {
-        let currentMonthDateDiv = document.createElement("div")
-        currentMonthDateDiv.className = "item other-month"
-        currentMonthDateDiv.textContent = date
-        calendarContainer.appendChild(currentMonthDateDiv)          
+        let currentMonthDateDiv = document.createElement("div");     // <div></div>
+        currentMonthDateDiv.className = "item other-month";    // <div class="item other-month"></div>
+        currentMonthDateDiv.textContent = date;    // <div class="item other-month">날짜</div>
+        calendarContainerDiv.appendChild(currentMonthDateDiv);    // <div id="calendar-continer"><div class="item">날짜</div></div>
     }
 
-    // 이번 달의 모든 날짜 표시
-
+    // 이번 달의 모든 날짜 표시하자
     for (let date = 1; date <= lastDateDate; date++) {
-        /// <div></div>
-        let currentMonthDateDiv = document.createElement("div")
-        
-        // <div class="item"></div>
-        currentMonthDateDiv.className = "item"
-
-        // <div class="item">날짜</div>
-        currentMonthDateDiv.textContent = date;
-
-        // <div id="calendar-container"><div class="item">날짜</div></div>
-        calendarContainer.appendChild(currentMonthDateDiv)
+        let currentMonthDateDiv = document.createElement("div");     // <div></div>
+        currentMonthDateDiv.className = "item";    // <div class="item"></div>
+        currentMonthDateDiv.dataset.date = `${year}-${month+1}-${date}`;    // <div class="item" data-date="년-월-일"></div>
+        currentMonthDateDiv.onclick = (event) => clickDate(event);    // <div class="item" data-date="년-월-일" onclick(event) => clickDate(event)></div>
+        currentMonthDateDiv.textContent = date;    // <div class="item">날짜</div>
+        calendarContainerDiv.appendChild(currentMonthDateDiv);    // <div id="calendar-continer"><div class="item">날짜</div></div>
     }
 
-    // 다음 달의 앞날짜 표시
+    // 다음 달의 앞 날짜 표시하자
+    for (let date = 1; date <= 6 - lastDay; date++) {
+        let currentMonthDateDiv = document.createElement("div");     // <div></div>
+        currentMonthDateDiv.className = "item other-month";    // <div class="item other-month"></div>
+        currentMonthDateDiv.textContent = date;    // <div class="item other-month">날짜</div>
+        calendarContainerDiv.appendChild(currentMonthDateDiv);    // <div id="calendar-continer"><div class="item">날짜</div></div>
+    }
 
-    for(let date = 1; date <= (6 - lastDay); date++){
-        let currentMonthDateDiv = document.createElement("div")
-        currentMonthDateDiv.className = "item other-month"
-        currentMonthDateDiv.textContent = date
-        calendarContainer.appendChild(currentMonthDateDiv)
-    }    
+
 }
 
-setCalendar(currentDate)
+changeMonth(0); // 현재 달 출력
+setCalendar(currentDate);   // 현재 달의 달력 보여주기
 
-changeMonth(0)
-
-
-const checkDone = document.querySelector('#board .btn')
-console.log(checkDone)
-
-checkDone.addEventListener('click', () => {
-    alert("예약완료")
-})
+const btns = document.getElementsByClassName("btn");
+for (let btn of btns) {
+    // 버튼 누르면 예약완료 창 뜨도록 도와주는 js
+    btn.onclick = () => alert('예약 완료');
+}
--- 240513_schoolMarketDormitory/js/dormitory.js
+++ 240513_schoolMarketDormitory/js/dormitory.js
@@ -1,75 +1,111 @@
-// 각 페이지 요소 가져오자
-
+// 각 페이지 가져오기
 const calendarDiv = document.getElementById("calendar");
-const selectionWashingMachineTimeDiv = document.getElementById("selection-washingmachine-time");
-const selectionRoomNameTimeDiv = document.querySelector("#selection-room-name");
-const boardDiv = document.querySelector("#board");
+const selectionWashingmachineTimeDiv = document.getElementById("selection-washingmachine-time");
+const selectionRoomNameDiv = document.querySelector("#selection-room-name");
+const boarDiv = document.querySelector("#board");
 
-const pageDivs = [calendarDiv, selectionWashingMachineTimeDiv, selectionRoomNameTimeDiv, boardDiv]
-const selectionItemDivs = document.getElementsByClassName('selection-item')
-console.log(selectionItemDivs);
+const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomNameDiv, boarDiv];
+
+const washingmachineSelect = document.getElementById("washingmachine");
+const timeSelect = document.getElementById("time");
+
+// 셀렉션 세개
+const selectionItemDivs = document.getElementsByClassName("selection-item");
 
 // calendarDiv.style.display = "block";
-// selectionWashingMachineTimeDiv.style.display = "block";
+// selectionWashingmachineTimeDiv.style.display = "block";
+// selectionRoomNameDiv.style.display = "block";
+// boarDiv.style.display = "block";
 
-let allData; // 모든 초기화 정보 : 세탁기, 시간, 호실 정보
-let weeklyReservation; // 미리 요일별로 지정된 예약 정보
-let newReservation; // 사용자가 입력하고 있는 예약 정보
-let reservations; // 사용자가 에약 완료한 정보들 저장
+let allData; //모든 초기화 정보 세탁기, 시간, 호실 정보
+let weeklyReservation; //미리 요일별로 예약된 정보
+let newResvation; //사용자가 입력하고 있는 예약 정보
+let reservations; //사용자가 예약 완료한 정보들
 
 const initData = async () => {
     // allData 가져오기
     const getAllData = async (url) => {
         return fetch(url)
-        .then(res => res.json())
-        .then(data => data)
-        .catch(err => console.log(err))
+            .then(response => response.json())
+            .then((data => { return data }))
+            .catch(error => console.error(error.message));
     }
+
     // weeklyReservation 가져오기
     const getWeeklyReservation = async (url) => {
         try {
-            const response = await fetch(url)
-            const data = await response.json()
-            return data
+            const response = await fetch(url); // await 추가
+            const data = await response.json();
+            return data;
         } catch (error) {
-            console.log(error)
+            console.error(error.message);
         }
     }
 
-    allData = await getAllData("js/allData.json")
-    console.log(allData)
-    weeklyReservation = await getWeeklyReservation("js/weekly-reservation.json")
-    console.log(weeklyReservation)
+    // allData 호출 (구현 내용은 비어 있음)
+    const allData = await getAllData("js/allData.json"); // 비어있는 함수 호출
+    // weeklyReservation 호출
+    const weeklyReservations = await getWeeklyReservation("js/weekly-reservation.json");
 }
-
-
-
-
 
 const setPage = (page) => {
-    // clear select
-    // selectionItemDivs.toArray().forEach(selectionItem => {
-    //     selectionItem.classList.remove("select")
-    // })
-
-    // 버튼 클릭 조절
-    for(selectionItem of selectionItemDivs){
-        selectionItem.classList.remove("select")
+    //clear selection
+    for (const selectionItemDiv of selectionItemDivs) {
+        selectionItemDiv.classList.remove("select");
+    }
+    //select selection
+    if (selectionItemDivs.length >= page) {  // 4page selection은 없음
+        selectionItemDivs[page - 1].classList.add("select");
     }
 
-    // real select
-    if(selectionItemDivs.length >= page) { // 4페이지 selection은 없어서 undefined 에러 해결하기 위해서
-        selectionItemDivs[page-1].classList.add("select")
-    }
-    
 
+    // clear pagas
     pageDivs.forEach(pageDiv => {
-        pageDiv.style.display = "none";
+        pageDiv.style.display = "none"; // 모든 페이지 안 보이게 처리
     })
 
-    pageDivs[page-1].style.display = "block";
+    // show pagas
+    pageDivs[page - 1].style.display = "block";
+
+    if (page === 2) {    //세탁기 ,, 시간
+        // 1,2,3 세탁기 / 1,2,3 시간 초기화
+        let allWashingmachineTime = {"1": ["1", "2", "3"], "2": ["1", "2", "3"], "3": ["1", "2", "3"]};
+        // 클릭한 날짜의 요일 구하기
+        // 예약된 시간을 확인하고, 세탁기가 있으면 초기화에서 제외
+        // 사용자가 예약한 예약을 보고 예약된 세탁기, 시간이 있으면 초기화 항목에서 제외
+        // 초기화 항목에서 예약된 시간 뺀 후, 모든 시간이 없는 세탁기 제외
+        // 세탁기 select에 option 만들기
+        let washingmachines = Object.keys(allWashingmachineTime);   //key만 가져옴
+        washingmachines.forEach(washingmachine => {
+            let newOption = document.createElement("option");   //<option></>
+            newOption.value = washingmachine;   //<option value="세탁기번호"></>
+            newOption.textContent = `${washingmachine}번 세탁기`;    //<option value="세탁기번호">세탁기번호 세탁기</>
+            washingmachineSelect.appendChild(newOption);    //washingmachineSelect에 자식으로 넣자
+        });
+        // 시간 select에 option 만들기
+        // [다음] 클릭 => 세탁기 번호, 시간 번호 보관 => setPage(3)
+    } else if (page === 3) {    //호실, 이름
+
+    } else if (page === 4) {    //세탁기 예약 현황표
+
+    }
 }
 
+const clickDate = (event) => {
+    console.log(event);
+    console.log(event.target.textContent);
+    console.log(event.target.dataset.date); //div에 클래스에 아이템에 아이디 뭐시기...텍스트 뭐시기 뽑음.
+    newResvation = {
+        "date": undefined,
+        "washingmachine": undefined,
+        "time": undefined,
+        "room": undefined,
+        "name": undefined,
+        "notification": undefined,
+    }
+    newResvation.date = event.target.dataset.date;  //클릭한 날짜 정보 새 예약에 기록하기
+    setPage(2);
+}
 
-setPage(1)
-initData()
+initData();
+setPage(1);
